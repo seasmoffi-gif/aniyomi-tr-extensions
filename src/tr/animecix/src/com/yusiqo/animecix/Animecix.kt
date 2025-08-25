683bffd8be92bb5b95538f0c0f52151863920590
seasmoffi-gif <seasmoffi@gmail.com>
2025-08-25 20:10:05 +0400

    Update Animecix.kt

--- src/tr/animecix/src/com/yusiqo/animecix/Animecix.kt
+++ src/tr/animecix/src/com/yusiqo/animecix/Animecix.kt
@@ -7,12 +7,12 @@ import eu.kanade.tachiyomi.animesource.model.SEpisode
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
 import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import okhttp3.Request
 import okhttp3.Response
-import eu.kanade.tachiyomi.util.asJsoup
 
 class Animecix : AnimeHttpSource() {
 
