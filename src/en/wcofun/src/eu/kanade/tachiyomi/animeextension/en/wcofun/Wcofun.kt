ead0fff4b7b699a18c5d3d7e5a9ef75f75b10614
seasmoffi-gif <seasmoffi@gmail.com>
2025-08-25 18:31:26 +0400

    Delete src/en directory

--- src/en/wcofun/src/eu/kanade/tachiyomi/animeextension/en/wcofun/Wcofun.kt
+++
@@ -1,8 +0,0 @@
-package eu.kanade.tachiyomi.animeextension.en.wcofun
-
-import eu.kanade.tachiyomi.multisrc.wcotheme.WcoTheme
-
-class Wcofun : WcoTheme() {
-    override val name = "Wcofun"
-    override val baseUrl = "https://www.wcoflix.tv"
-}
